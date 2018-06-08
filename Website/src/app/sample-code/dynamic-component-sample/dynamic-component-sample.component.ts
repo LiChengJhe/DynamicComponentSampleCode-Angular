35f56c69f278fa58316ec2c518d10462608019e3
李承哲(ChengjheLi) <chengjheli@chipbond.com.tw>
2018-06-08 09:35:07 +0800

    modify sample-code/dynamic-component-sample/dynamic-component-sample.component.ts

--- Website/src/app/sample-code/dynamic-component-sample/dynamic-component-sample.component.ts
+++ Website/src/app/sample-code/dynamic-component-sample/dynamic-component-sample.component.ts
@@ -14,49 +14,43 @@ declare var toastr: any;
   styleUrls: ['./dynamic-component-sample.component.css']
 })
 export class DynamicComponentSampleComponent implements OnInit, AfterViewChecked {
-
   @ViewChild('Container', { read: ViewContainerRef }) _Container: ViewContainerRef;
-  private _Notes = [];
-  private _NoteCount = 0;
-  private _Component: NoteComponent;
+  private _NoteComponents = [];
+  private _NoteComponentCount = 0;
+  private _CurNoteComponent: NoteComponent;
   constructor(private componentFactory: ComponentFactoryResolver) {
   }
-
   ngOnInit() {
     this.AddNoteComponent();
   }
-
   AddNoteComponent(): void {
     const componentFactory = this.componentFactory.resolveComponentFactory(NoteComponent);
     const componentRef: ComponentRef<NoteComponent> = this._Container.createComponent(componentFactory);
-    this._Component = componentRef.instance;
-    this._Component.NoteID = this._NoteCount;
-    this._NoteCount++;
+    this._CurNoteComponent = componentRef.instance;
+    this._CurNoteComponent.NoteID = this._NoteComponentCount;
+    this._NoteComponentCount++;
   }
-
   GetAllNotes() {
     const dataSet: any[] = [];
-    for (const i of this._Notes) {
+    for (const i of this._NoteComponents) {
       dataSet.push(i.Component.Form.value);
     }
-
     toastr.options = {
       positionClass: 'toast-bottom-left'
     };
     toastr.warning(JSON.stringify(dataSet));
   }
-
   ngAfterViewChecked(): void {
-    if (!_.find(this._Notes, (i) => i.Component.NoteID == this._Component.NoteID)) {
-      this._Notes.push({
-        Component: this._Component,
+    if (!_.find(this._NoteComponents, (i) => i.Component.NoteID === this._CurNoteComponent.NoteID)) {
+      this._NoteComponents.push({
+        Component: this._CurNoteComponent,
         DelNoteEvent: fromEvent(
-          $('button[noteid="' + this._Component.NoteID + '"]'), 'click')
+          $('button[noteid="' + this._CurNoteComponent.NoteID + '"]'), 'click')
           .pipe(take(1)).subscribe((e: MouseEvent) => {
             const noteID: number = $(e.target).attr('noteid') as number;
             $.each($('button[noteid]'), (index, val) => {
-              if ($(val).attr('noteid') == noteID) {
-                this._Notes = this._Notes.filter((i) => Number(i.Component.NoteID)  != noteID);
+              if ($(val).attr('noteid') === noteID) {
+                this._NoteComponents = this._NoteComponents.filter((i) => Number(i.Component.NoteID)  !== noteID);
                 this._Container.remove(index);
                 return false;
               }
--- Website/src/app/sample-code/note/note.component.ts
+++ Website/src/app/sample-code/note/note.component.ts
@@ -1,29 +1,22 @@
 import { Component, OnInit} from '@angular/core';
 import { FormGroup, FormControl, FormArray } from '@angular/forms';
-
-
 @Component({
   selector: 'app-note',
   templateUrl: './note.component.html',
   styleUrls: ['./note.component.css']
 })
 export class NoteComponent implements OnInit {
-
   Form: FormGroup;
   NoteID: number;
-
   constructor( ) {
   }
-
   ngOnInit() {
     this.InitForm();
   }
-
   InitForm(): void {
     this.Form = new FormGroup({
       Title: new FormControl(null),
       Content: new FormControl(null)
     });
   }
-
 }
